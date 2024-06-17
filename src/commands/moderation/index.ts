0dc9685ea870127fd87308613ae754a021604f7f
encode42 <me@encode42.dev>
2024-06-17 17:27:24 -0400

    Remove currently unimplemented command

--- src/commands/moderation/index.ts
+++ src/commands/moderation/index.ts
@@ -1,10 +1,9 @@
 import type { CategoryMeta } from "~/types/Command.ts";
 
-export * from "./giveaway";
 export * from "./unverify";
 export * from "./verify";
 
 export const meta: CategoryMeta = {
 	"label": "test",
 	"description": "test"
-}
+};
